794f55e5b31b733fe3d24564df9f8609c824d645
Danilo Torrisi <danilotorrisi@keix.com>
2023-12-05 12:40:23 +0100

    Wokring on agenda schema.

--- app/dialogs/agenda.tsx
+++ app/dialogs/agenda.tsx
@@ -1,17 +1,17 @@
 import {
-  ClockIcon,
-  PencilSquareIcon,
-  PlusCircleIcon,
-  PlusIcon,
-  ReceiptPercentIcon,
-  XMarkIcon,
+	ClockIcon,
+	PencilSquareIcon,
+	PlusCircleIcon,
+	PlusIcon,
+	ReceiptPercentIcon,
+	XMarkIcon,
 } from "@heroicons/react/24/solid";
 import {
-  ServiceType,
-  type Agenda,
-  type Doctor,
-  ClinicalService,
-  ServiceOffering,
+	ServiceType,
+	type Agenda,
+	type Doctor,
+	ClinicalService,
+	ServiceOffering,
 } from "@prisma/client";
 import type { WithSerializedTypes } from "~/utils/client";
 import Button from "~/components/button";
@@ -35,137 +35,137 @@ import classNames from "classnames";
 import Field from "~/components/field";
 
 export function AgendaDialog(p: {
-  agenda?: WithSerializedTypes<Agenda | null>;
-  doctors: Doctor[];
-  services: WithSerializedTypes<
-    ServiceOffering & { service: ClinicalService }
-  >[];
-  redirectTo?: string;
-  clinicId: string;
-  isOpen: boolean;
-  onClose?: () => void;
+	agenda?: WithSerializedTypes<Agenda | null>;
+	doctors: Doctor[];
+	services: WithSerializedTypes<
+		ServiceOffering & { service: ClinicalService }
+	>[];
+	redirectTo?: string;
+	clinicId: string;
+	isOpen: boolean;
+	onClose?: () => void;
 }) {
-  const isNew = p.agenda == null;
-  const [selectedTab, setSelectedTab] = useState("info");
-  const matches = useMatches();
-  const redirectTo = p.redirectTo ?? last(matches)?.pathname;
-  let agenda = !isNew ? { ...p.agenda } : null;
-  if (agenda != null) {
-    agenda.validFrom =
-      agenda.validFrom != null
-        ? DateTime.fromISO(agenda.validFrom).toISODate()
-        : null;
-    agenda.validUntil =
-      agenda.validUntil != null
-        ? DateTime.fromISO(agenda.validUntil).toISODate()
-        : null;
-  }
+	const isNew = p.agenda == null;
+	const [selectedTab, setSelectedTab] = useState("info");
+	const matches = useMatches();
+	const redirectTo = p.redirectTo ?? last(matches)?.pathname;
+	let agenda = !isNew ? { ...p.agenda } : null;
+	if (agenda != null) {
+		agenda.validFrom =
+			agenda.validFrom != null
+				? DateTime.fromISO(agenda.validFrom).toISODate()
+				: null;
+		agenda.validUntil =
+			agenda.validUntil != null
+				? DateTime.fromISO(agenda.validUntil).toISODate()
+				: null;
+	}
 
-  return (
-    <Overlay isOpen={p.isOpen}>
-      <div className="card w-3/5 z-10">
-        <h2 className="border-none">
-          {isNew ? "Aggiungi Agenda" : "Modifica Agenda"}
-        </h2>
-        <div className="flex bg-gray-50 divide-gray-200 divide-y-0 divide-x  items-stretch text-sm font-semibold -mt-4 mb-2 border  rounded overflow-hidden">
-          <Tab
-            selectedTab={selectedTab}
-            tab={{
-              id: "info",
-              title: "Informazioni",
-              icon: <PencilSquareIcon />,
-            }}
-            onClick={() => setSelectedTab("info")}
-          />
-          <Tab
-            selectedTab={selectedTab}
-            tab={{
-              id: "timeslots",
-              title: "Disponibilità",
-              icon: <ClockIcon />,
-            }}
-            onClick={() => setSelectedTab("timeslots")}
-          />
-          <Tab
-            selectedTab={selectedTab}
-            tab={{
-              id: "services",
-              title: "Prestazioni",
-              icon: <ReceiptPercentIcon />,
-            }}
-            onClick={() => setSelectedTab("services")}
-          />
-        </div>
+	return (
+		<Overlay isOpen={p.isOpen}>
+			<div className="card w-3/5 z-10">
+				<h2 className="border-none">
+					{isNew ? "Aggiungi Agenda" : "Modifica Agenda"}
+				</h2>
+				<div className="flex bg-gray-50 divide-gray-200 divide-y-0 divide-x  items-stretch text-sm font-semibold -mt-4 mb-2 border  rounded overflow-hidden">
+					<Tab
+						selectedTab={selectedTab}
+						tab={{
+							id: "info",
+							title: "Informazioni",
+							icon: <PencilSquareIcon />,
+						}}
+						onClick={() => setSelectedTab("info")}
+					/>
+					<Tab
+						selectedTab={selectedTab}
+						tab={{
+							id: "timeslots",
+							title: "Disponibilità",
+							icon: <ClockIcon />,
+						}}
+						onClick={() => setSelectedTab("timeslots")}
+					/>
+					<Tab
+						selectedTab={selectedTab}
+						tab={{
+							id: "services",
+							title: "Prestazioni",
+							icon: <ReceiptPercentIcon />,
+						}}
+						onClick={() => setSelectedTab("services")}
+					/>
+				</div>
 
-        <XMarkIcon className="close-button" onClick={p.onClose} />
+				<XMarkIcon className="close-button" onClick={p.onClose} />
 
-        <ValidatedForm
-          method="post"
-          key={p.agenda?.id ?? v4()}
-          validator={validator}
-          resetAfterSubmit={true}
-          defaultValues={agenda}
-          encType="multipart/form-data"
-          action="/agendas/upsert"
-        >
-          <DialogCloseOnSubmit onClose={p.onClose} />
-          <input
-            type="hidden"
-            value={isNew ? "create" : "update"}
-            name="_action"
-          />
-          <input type="hidden" name="_redirect" value={redirectTo} />
-          <input type="hidden" name="_id" value={p.agenda?.id} />
-          <input
-            type="hidden"
-            name="clinicId"
-            value={p.agenda?.clinicId ?? p.clinicId}
-          />
-          <div
-            className={classNames("form-grid px-4 pt-4", {
-              hidden: selectedTab !== "info",
-            })}
-          >
-            <InputField name="name" label="Nome" />
-            <ServiceTypeField />
-            <DoctorField doctors={p.doctors} />
-            <DateRangeFields
-              type="date"
-              name={["validFrom", "validUntil"]}
-              label="Valido dal"
-            />
-            <InputField
-              inputProps={{ type: "number" }}
-              name="slotInterval"
-              label="Slot di Prenotazione"
-            />
-          </div>
-          <div
-            className={classNames("form-grid px-4 pt-4", {
-              hidden: selectedTab !== "services",
-            })}
-          >
-            <ServicesField name="services" options={p.services} />
-          </div>
+				<ValidatedForm
+					method="post"
+					key={p.agenda?.id ?? v4()}
+					validator={validator}
+					resetAfterSubmit={true}
+					defaultValues={agenda}
+					encType="multipart/form-data"
+					action="/agendas/upsert"
+				>
+					<DialogCloseOnSubmit onClose={p.onClose} />
+					<input
+						type="hidden"
+						value={isNew ? "create" : "update"}
+						name="_action"
+					/>
+					<input type="hidden" name="_redirect" value={redirectTo} />
+					<input type="hidden" name="_id" value={p.agenda?.id} />
+					<input
+						type="hidden"
+						name="clinicId"
+						value={p.agenda?.clinicId ?? p.clinicId}
+					/>
+					<div
+						className={classNames("form-grid px-4 pt-4", {
+							hidden: selectedTab !== "info",
+						})}
+					>
+						<InputField name="name" label="Nome" />
+						<ServiceTypeField />
+						<DoctorField doctors={p.doctors} />
+						<DateRangeFields
+							type="date"
+							name={["validFrom", "validUntil"]}
+							label="Valido dal"
+						/>
+						<InputField
+							inputProps={{ type: "number" }}
+							name="slotInterval"
+							label="Slot di Prenotazione"
+						/>
+					</div>
+					<div
+						className={classNames("form-grid px-4 pt-4", {
+							hidden: selectedTab !== "services",
+						})}
+					>
+						<ServicesField name="services" options={p.services} />
+					</div>
 
-          <div
-            className={classNames("form-grid px-4 pt-4", {
-              hidden: selectedTab !== "timeslots",
-            })}
-          >
-            <Field label="">
-          </div>
-          <div className="p-4 pb-2">
-            <Button
-              intent="primary"
-              className="w-full"
-              type="submit"
-              text={isNew ? "Aggiungi Agenda" : "Modifica Agenda"}
-              icon={<PlusIcon />}
-            />
-          </div>
-        </ValidatedForm>
-      </div>
-    </Overlay>
-  );
+					<div
+						className={classNames("form-grid px-4 pt-4", {
+							hidden: selectedTab !== "timeslots",
+						})}
+					>
+						A
+					</div>
+					<div className="p-4 pb-2">
+						<Button
+							intent="primary"
+							className="w-full"
+							type="submit"
+							text={isNew ? "Aggiungi Agenda" : "Modifica Agenda"}
+							icon={<PlusIcon />}
+						/>
+					</div>
+				</ValidatedForm>
+			</div>
+		</Overlay>
+	);
 }
--- app/services/booking.server.ts
+++ app/services/booking.server.ts
@@ -4,60 +4,87 @@ import { validator } from "~/validators/booking";
 import { validationError } from "remix-validated-form";
 import { db } from "~/utils/db.server";
 import { date } from "zod";
+import Twillio from "twilio";
+import { getDisplayName } from "~/utils/patient";
+import { DateTime } from "luxon";
+
+const FROM_TWILLIO = process.env.TWILLIO_BOOKING_FROM;
+const client = Twillio(
+	process.env.TWILLIO_ACCOUNT_SID,
+	process.env.TWILLIO_AUTH_TOKEN,
+);
 
 export async function handleRequest(request: Request) {
-  const form = await parseMultipartFormData(request);
+	const form = await parseMultipartFormData(request);
 
-  const user = await authenticator.isAuthenticated(request, {
-    failureRedirect: "/login",
-  });
+	const user = await authenticator.isAuthenticated(request, {
+		failureRedirect: "/login",
+	});
 
-  // Validate
-  const result = await validator.validate(form);
-  if (result.error) {
-    return validationError(result.error);
-  }
+	// Validate
+	const result = await validator.validate(form);
+	if (result.error) {
+		return validationError(result.error);
+	}
 
-  const { data } = result;
-  const {
-    firstName,
-    lastName,
-    emailAddress,
-    phoneNumber,
-    date,
-    serviceId,
-    agendaId,
-  } = data;
+	const { data } = result;
+	const {
+		firstName,
+		lastName,
+		emailAddress,
+		phoneNumber,
+		date,
+		serviceId,
+		agendaId,
+	} = data;
 
-  // First let's get the service.
-  const service = await db.serviceOffering.findUnique({
-    where: {
-      id: serviceId,
-    },
-  });
-  if (service == null) {
-    throw new Error("Service not found");
-  }
+	// First let's get the service.
+	const service = await db.serviceOffering.findUnique({
+		where: {
+			id: serviceId,
+		},
+		include: {
+			service: true,
+			clinic: true,
+		},
+	});
+	if (service == null) {
+		throw new Error("Service not found");
+	}
 
-  const patient = await db.patient.create({
-    data: {
-      firstName,
-      lastName,
-      emailAddress,
-      phoneNumber,
-    },
-  });
+	const patient = await db.patient.create({
+		data: {
+			firstName,
+			lastName,
+			emailAddress,
+			phoneNumber,
+		},
+	});
 
-  // Let's create the booking.
-  const booking = await db.serviceBooking.create({
-    data: {
-      patientId: patient.id,
-      bookedAt: date,
-      duration: service.duration,
-      serviceId: service.id,
-      agendaId,
-    },
-  });
+	// Let's create the booking.
+	await db.serviceBooking.create({
+		data: {
+			patientId: patient.id,
+			bookedAt: date,
+			duration: service.duration,
+			serviceId: service.id,
+			agendaId,
+		},
+	});
 
-  return `/booking/${booking.id}/confirmed`;
+	// Send the confrimation message.
+	const res = await client.messages.create({
+		contentSid: process.env.TWILLIO_BOOKING_TEMPLATE_SID,
+		from: process.env.TWILLIO_BOOKING_FROM,
+		contentVariables: JSON.stringify({
+			1: getDisplayName({ firstName, lastName }),
+			2: service.service.name,
+			3: service.clinic.name,
+			4: `${service.clinic.address}, ${service.clinic.city}`,
+			5: DateTime.fromISO(date).toLocaleString(DateTime.DATE_MED),
+			6: DateTime.fromISO(date).toLocaleString(DateTime.TIME_24_SIMPLE),
+		}),
+		to: `whatsapp:+39${phoneNumber}`,
+	});
+	console.log(res);
 }
