8ea1ffe9fafde83ac2d7d9b479c2b3164f5b4c7b
Danilo Torrisi <danilotorrisi@keix.com>
2023-12-13 10:06:04 +0100

    Improved salus.

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -1,8 +1,19 @@
-import ColorLogo from "../../public/logo.svg";
+import ColorLogo from "../../public/salus_shit.svg";
 import MonoLogo from "../../public/mono.svg";
+import ColorNotShitLogo from "../../public/salus_less_not_shit.svg";
+import MonoNotShitLogo from "../../public/salus_mono_less_not_shit.svg";
 
-export default function Logo(p: { mono?: boolean; className?: string }) {
-  const mono = p.mono ?? false;
-  const src = mono ? MonoLogo : ColorLogo;
-  return <img src={src} className={p.className} />;
+export default function Logo(p: {
+	mono?: boolean;
+	shit?: boolean;
+	className?: string;
+}) {
+	const mono = p.mono ?? false;
+	let src;
+	if (p.shit) {
+		src = mono ? MonoLogo : ColorLogo;
+	} else {
+		src = mono ? MonoNotShitLogo : ColorNotShitLogo;
+	}
+	return <img src={src} className={p.className} />;
 }
