b66f68731d547ce0a6b670a544f2b35def48d29a
Danilo Torrisi <danilotorrisi@keix.com>
2023-12-13 12:18:36 +0100

    Trying to remove phoneInputField.

--- app/components/fields/phoneInputField.tsx
+++ app/components/fields/phoneInputField.tsx
@@ -4,41 +4,44 @@ import SimpleCalendar, {
   SimpleCalendarOptions,
 } from "../simpleCalendar";
 import Field from "../field";
-import PhoneInput, { Value } from "react-phone-number-input";
-import "react-phone-number-input/style.css";
+// import PhoneInput, { Value } from "react-phone-number-input";
+// import "react-phone-number-input/style.css";
 import React from "react";
 import classNames from "classnames";
+
+type Value = {};
 
 export default function PhoneInputField(p: { name: string; label: string }) {
   const [value, setValue] = useControlField<Value | undefined>(p.name);
   console.log(value);
   return (
     <Field name={p.name} label={p.label}>
-      <PhoneInput
+      <div>AA</div>
+      {/* <PhoneInput
         value={value}
         onChange={setValue}
         defaultCountry="IT"
         inputComponent={CustomInput}
-      />
+      /> */}
     </Field>
   );
 }
 
-const CustomInput = React.forwardRef<HTMLInputElement>(
-  (
-    p: React.DetailedHTMLProps<
-      React.InputHTMLAttributes<HTMLInputElement>,
-      HTMLInputElement
-    >,
-    ref
-  ) => {
-    return (
-      <input
-        ref={ref}
-        {...p}
-        name="phoneNumber"
-        className={classNames(p.className, "input")}
-      />
-    );
-  }
-);
+// const CustomInput = React.forwardRef<HTMLInputElement>(
+//   (
+//     p: React.DetailedHTMLProps<
+//       React.InputHTMLAttributes<HTMLInputElement>,
+//       HTMLInputElement
+//     >,
+//     ref
+//   ) => {
+//     return (
+//       <input
+//         ref={ref}
+//         {...p}
+//         name="phoneNumber"
+//         className={classNames(p.className, "input")}
+//       />
+//     );
+//   }
+// );
