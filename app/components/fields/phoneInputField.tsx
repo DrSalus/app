a4559c83db1fff146f6b44b78341cca3a09e5f3c
Danilo Torrisi <danilotorrisi@keix.com>
2023-12-13 14:22:39 +0100

    Trying to adde phone number field.

--- app/components/fields/phoneInputField.tsx
+++ app/components/fields/phoneInputField.tsx
@@ -1,47 +1,41 @@
 import { useControlField } from "remix-validated-form";
-import SimpleCalendar, {
-  CalendarSlot,
-  SimpleCalendarOptions,
-} from "../simpleCalendar";
 import Field from "../field";
-// import PhoneInput, { Value } from "react-phone-number-input";
-// import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
+import type { Value } from "react-phone-number-input";
+import "react-phone-number-input/style.css";
 import React from "react";
 import classNames from "classnames";
-
-type Value = {};
 
 export default function PhoneInputField(p: { name: string; label: string }) {
   const [value, setValue] = useControlField<Value | undefined>(p.name);
   console.log(value);
   return (
     <Field name={p.name} label={p.label}>
-      <div>AA</div>
-      {/* <PhoneInput
+      <PhoneInput
         value={value}
         onChange={setValue}
         defaultCountry="IT"
         inputComponent={CustomInput}
-      /> */}
+      />
     </Field>
   );
 }
 
-// const CustomInput = React.forwardRef<HTMLInputElement>(
-//   (
-//     p: React.DetailedHTMLProps<
-//       React.InputHTMLAttributes<HTMLInputElement>,
-//       HTMLInputElement
-//     >,
-//     ref
-//   ) => {
-//     return (
-//       <input
-//         ref={ref}
-//         {...p}
-//         name="phoneNumber"
-//         className={classNames(p.className, "input")}
-//       />
-//     );
-//   }
-// );
+const CustomInput = React.forwardRef<HTMLInputElement>(
+  (
+    p: React.DetailedHTMLProps<
+      React.InputHTMLAttributes<HTMLInputElement>,
+      HTMLInputElement
+    >,
+    ref
+  ) => {
+    return (
+      <input
+        ref={ref}
+        {...p}
+        name="phoneNumber"
+        className={classNames(p.className, "input")}
+      />
+    );
+  }
+);
